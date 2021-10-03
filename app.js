b0a5d36d562e41a1861a3adc1ae92a16b39055a4
Jane Jeyranyan <73958620+janejey@users.noreply.github.com>
2021-10-03 18:10:51 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let button = document.getElementById("button");
+let inputValue = document.getElementById("inputValue");
+let cityName = document.getElementById("name");
+let desc = document.getElementById("desc");
+let temp = document.getElementById("temp");
+
+button.addEventListener('click', function() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=3b255a10290a4d93c9b23dedfdba66ac')
+.then(response => response.json())
+.then(data => {
+    let nameValue = data['name'];
+    let tempValue = "" + data['main']['temp'] + '°F';
+    let descValue = data['weather'][0]['description'];
+    
+    cityName.innerHTML = nameValue;
+    temp.innerHTML = tempValue;
+    desc.innerHTML = descValue;
+})
+
+.catch(err => alert('Wrong city name'))
+})
